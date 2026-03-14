b732c51aedd4bb8a930f4a6aa730642ee44dacbf
johannke <johann.kelsch@dlr.de>
2026-03-14 20:57:05 +0100

    reworking tools

--- tools/test_cuda.py
+++ tools/test_cuda.py
@@ -1,6 +1,8 @@
-import torch
-print(torch.__version__)
-print("CUDA available:", torch.cuda.is_available())
-print("CUDA devices:", torch.cuda.device_count())
-for i in range(torch.cuda.device_count()):
-    print(i, torch.cuda.get_device_name(i))
+try:
+    import torch as _torch
+    print("CUDA available:", _torch.cuda.is_available())
+    print("CUDA devices:", _torch.cuda.device_count())
+    for i in range(_torch.cuda.device_count()):
+        print(i, _torch.cuda.get_device_name(i))
+except Exception:
+    print("CUDA not available:")
